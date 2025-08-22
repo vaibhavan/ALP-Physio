3a433e656d83a9becacc903e6b0ea2e4d264c862
Vaibhavan M <167299616+vaibhavan@users.noreply.github.com>
2025-08-22 13:04:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleFAQ(element) {
+  const answer = element.nextElementSibling;
+  if (answer.style.display === "block") {
+    answer.style.display = "none";
+  } else {
+    answer.style.display = "block";
+  }
+}
+  
